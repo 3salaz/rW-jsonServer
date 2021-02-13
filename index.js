d84ce6f3e58f46c96137c7e373ce4ef22126cb90
Erik Salazar <3salaz.dev@gmail.com>
2021-02-12 17:15:50 -0800

    added db.json

--- index.js
+++ index.js
@@ -7,5 +7,21 @@ const bodyParser = require("body-parser");
 const db = lowDb(new FileSync('db.json'))
 
 db.defaults({
-    squareSpaceData: []
+    videoLib: []
 }).write()
+
+// initializing server
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json()); 
+const PORT = 4000;
+
+app.get('/videoLib', (req,res) => {
+    const data = db.get("videoLib").value()
+    return res.json(data)
+})
+
+app.listen(PORT, () => {
+    console.log(`Running on port http://localhost:${PORT}/videoLib`)
+})
