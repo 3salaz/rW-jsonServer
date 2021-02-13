aa12d153f9b459d5456c6c05d9d5ab21f293004d
Erik Salazar <3salaz.dev@gmail.com>
2021-02-12 19:45:01 -0800

    added gitignore for eb files

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const lowDb = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
 const bodyParser = require("body-parser");
+const { nanoid } = require("nanoid");
 
 const db = lowDb(new FileSync('db.json'))
 
@@ -17,11 +18,19 @@ app.use(cors());
 app.use(bodyParser.json()); 
 const PORT = 4000;
 
-app.get('/videoLib', (req,res) => {
-    const data = db.get("videoLib").value()
+app.get('/videos', (req,res) => {
+    const data = db.get("videos").value()
     return res.json(data)
 })
 
+app.post('/videos/new', (req,res) => {
+    const video = req.body
+    db.get("videos").push({
+        ... video, id: nanoid()
+    }).write()
+    res.json({ success: true})
+})
+
 app.listen(PORT, () => {
-    console.log(`Running on port http://localhost:${PORT}/videoLib`)
+    console.log(`Running on port http://localhost:${PORT}/videos`)
 })
